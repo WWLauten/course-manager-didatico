3850d09d84c3141c6a4885383b04c0d2930748ea
Willian W. Lautenschlager <willianw@uol.com.br>
2021-01-09 17:03:12 -0300

    Módulo 2 aula 6

--- src/app/courses/courses-info.component.ts
+++ src/app/courses/courses-info.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component ({
     templateUrl: './courses-info.component.html'
 })
 export class CourseInfoComponent implements OnInit {
     
-    courseId: number = 0;
+    course!: Course;
 
-    constructor(private activedRoute: ActivatedRoute) { }
+    constructor(private activedRoute: ActivatedRoute, private courseService: CourseService) { }
 
     ngOnInit(): void {
         //If you are absolutely sure that the paramMap.get DOES exist in your DOM, 
         //you can show TS your confidence with a ! operator.
         // Notice the "!" at the end of line
-        this.courseId = +this.activedRoute.snapshot.paramMap.get('id')!;
+        this.course = this.courseService.retrieveById(+this.activedRoute.snapshot.paramMap.get('id')!);
+    }
+
+    save(): void {
+        this.courseService.save(this.course);
     }
 
 }
